fb8546fe117912be87d06d0fcfdeb30ee5936ce2
mariabrock <mariabrock414@gmail.com>
2019-09-07 19:55:50 -0500

    started javascript for print funtions

--- main.js
+++ main.js
@@ -46,3 +46,25 @@ const createProjectsCards = (projectArray) => {
 
 
 createProjectsCards(projects);
+
+const getBio = () => {
+    let domString = `
+    <div class="card-body">
+            <p>Hi. My name's Maria. I'm a programmer and graphic designer who has a flair for anything handmade. If it's from space, I think it's cool. If you offer me coffee, you'll be my new best friend.</p> 
+
+            <p>I have a background of 15 years of experience in diverse types of customer service industries. I've done eveything from making sandwiches to running said sandwich store. I've got a little corporate life under my  belt too. I'm currently working as a Project Manager for B2B corporate payment conversion campaigns.  I have a degree in Graphic Design from Western Kentucky University that I keep alive by illustrating/handlettering and crocheting in my free time.  I'm a puzzle solver, gamer, and free spirit who spends all the time I can with my Husband and my family.</p> 
+                
+            <p>I love a good challenge. Programming appeals to me because it combines so many things I love (and that I'm good at!). Each different language is a new opportunity to learn, and each new project puzzle is a chance to step outside my comfort zone!</p>
+    </div>`
+    printToDOM(domString, 'bioPage')
+  };
+
+const buttonClick = (e) =>{
+        const buttonType = e.target.id;
+        if (buttonType === 'nav-item nav-link')
+        getBio()
+    };
+
+document.getElementById('bio').addEventListener('click', buttonClick);
+document.getElementById('tech').addEventListener('click', buttonClick);
+document.getElementById('projects').addEventListener('click', buttonClick);
