9d6a6ac5ae4b0a09987e8fbfef1add2f44ec05db
mariabrock <mariabrock414@gmail.com>
2019-09-09 22:58:26 -0500

    fixed the print to dom issues

--- main.js
+++ main.js
@@ -45,24 +45,31 @@ const createProjectsCards = (projectArray) => {
   };
 
 
-createProjectsCards(projects);
 
 const getBio = () => {
     let domString = `
-    <div class="card-body">
             <p>Hi. My name's Maria. I'm a programmer and graphic designer who has a flair for anything handmade. If it's from space, I think it's cool. If you offer me coffee, you'll be my new best friend.</p> 
 
             <p>I have a background of 15 years of experience in diverse types of customer service industries. I've done eveything from making sandwiches to running said sandwich store. I've got a little corporate life under my  belt too. I'm currently working as a Project Manager for B2B corporate payment conversion campaigns.  I have a degree in Graphic Design from Western Kentucky University that I keep alive by illustrating/handlettering and crocheting in my free time.  I'm a puzzle solver, gamer, and free spirit who spends all the time I can with my Husband and my family.</p> 
                 
             <p>I love a good challenge. Programming appeals to me because it combines so many things I love (and that I'm good at!). Each different language is a new opportunity to learn, and each new project puzzle is a chance to step outside my comfort zone!</p>
-    </div>`
-    printToDOM(domString, '#bioPage')
+    `;
+    printToDOM(domString, 'bioPage')
   };
 
 const buttonClick = (e) =>{
         const buttonType = e.target.id;
-        if (buttonType === 'nav-item nav-link')
-        getBio()
+        if (buttonType === 'bio') {
+            getBio();
+            document.getElementById('homePage').innerHTML = '';
+            document.getElementById('technologiesPage').innerHTML = '';
+            document.getElementById('project0sPage').innerHTML = '';
+        } else if (buttonType === 'projects') {
+            createProjectsCards(projects);
+            document.getElementById('homePage').innerHTML = '';
+            document.getElementById('technologiesPage').innerHTML = '';
+            document.getElementById('bioPage').innerHTML = '';
+        }
     };
 
 document.getElementById('home').addEventListener('click', buttonClick);
@@ -71,40 +78,40 @@ document.getElementById('tech').addEventListener('click', buttonClick);
 document.getElementById('projects').addEventListener('click', buttonClick);
 
 //this is the js for the SPA!
-const app = {
-    pages: [],
-    show: new Event('show'),
-    init: function(){
-        app.pages = document.querySelectorAll('.fullPage');
-        app.pages.forEach((pg)=>{
-            pg.addEventListener('show', app.pageShown);
-        })
+// const app = {
+//     pages: [],
+//     show: new Event('show'),
+//     init: function(){
+//         app.pages = document.querySelectorAll('.fullPage');
+//         app.pages.forEach((pg)=>{
+//             pg.addEventListener('show', app.pageShown);
+//         })
         
-        document.querySelectorAll('.nav-link').forEach((link)=>{
-            link.addEventListener('click', app.nav);
-        })
-        history.replaceState({}, 'Home', '#homePage');
-        window.addEventListener('popstate', app.poppin);
-    },
-    nav: function(ev){
-        ev.preventDefault();
-        let currentPage = ev.target.getAttribute('id');
-        document.querySelector('.active').classList.remove('active');
-        document.getElementById(currentPage).classList.add('active');
-        console.log(currentPage)
-        history.pushState({}, currentPage, `#${currentPage}`);
-        document.getElementById(currentPage).dispatchEvent(app.show);
-    },
+//         document.querySelectorAll('.nav-link').forEach((link)=>{
+//             link.addEventListener('click', app.nav);
+//         })
+//         history.replaceState({}, 'Home', '#homePage');
+//         window.addEventListener('popstate', app.poppin);
+//     },
+//     nav: function(ev){
+//         ev.preventDefault();
+//         let currentPage = ev.target.getAttribute('id');
+//         document.querySelector('.active').classList.remove('active');
+//         document.getElementById(currentPage).classList.add('active');
+//         console.log(currentPage)
+//         history.pushState({}, currentPage, `#${currentPage}`);
+//         document.getElementById(currentPage).dispatchEvent(app.show);
+//     },
     
-    poppin: function(ev){
-        console.log(location.hash, 'popstate event');
-        let hash = location.hash.replace('#' ,'');
-        document.querySelector('.active').classList.remove('active');
-        document.getElementById(hash).classList.add('active');
-        console.log(hash)
-        //history.pushState({}, currentPage, `#${currentPage}`);
-        document.getElementById(hash).dispatchEvent(app.show);
-    }
-}
+//     poppin: function(ev){
+//         console.log(location.hash, 'popstate event');
+//         let hash = location.hash.replace('#' ,'');
+//         document.querySelector('.active').classList.remove('active');
+//         document.getElementById(hash).classList.add('active');
+//         console.log(hash)
+//         //history.pushState({}, currentPage, `#${currentPage}`);
+//         document.getElementById(hash).dispatchEvent(app.show);
+//     }
+// }
 
-document.addEventListener('DOMContentLoaded', app.init);
+// document.addEventListener('DOMContentLoaded', app.init);
